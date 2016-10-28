ec1019fc4ee6e3db268bf35a27fbd3ba95c89e36
eVookelj <evukelj@stuy.edu>
2016-10-28 17:59:29 -0400

    Make time printing work

--- stat.c
+++ stat.c
@@ -3,13 +3,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <time.h>
 
 int main() {
   struct stat buff;
   stat("stat.c", &buff);
-  int size = buff.st_size;
-  int mode = buff.st_mode;
-  int timeMod = buff.st_mtime;
-  printf("Size: %d\nMode: %d\nTime of last change: %d\n", size, mode, timeMod);
+  off_t size = buff.st_size;
+  mode_t mode = buff.st_mode;
+  time_t timeMod = buff.st_mtime;
+  
+  printf("Size: %d\nMode (last 3 digits = owner/group/others): %o\nTime of last change: %s\n", size, mode, ctime(&timeMod));
   return 0;
 }

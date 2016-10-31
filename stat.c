a2471d3502799e63e584f0deeebff1402227ad09
eVookelj <evukelj@stuy.edu>
2016-10-31 15:04:27 -0400

    Finish updated work

--- stat.c
+++ stat.c
@@ -11,7 +11,8 @@ int main() {
   off_t size = buff.st_size;
   mode_t mode = buff.st_mode;
   time_t timeMod = buff.st_mtime;
+  int statchmod = mode & (S_IRWXU | S_IRWXG | S_IRWXO);
   
-  printf("Size: %d\nMode (last 3 digits = owner/group/others): %o\nTime of last change: %s\n", size, mode, ctime(&timeMod));
+  printf("Size: %d bytes\nMode (last 3 digits = owner/group/others): %o\nTime of last change: %s\n", size, statchmod, ctime(&timeMod));
   return 0;
 }
